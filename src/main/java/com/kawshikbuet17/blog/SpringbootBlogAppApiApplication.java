ca13b287cf5ca3ed5ff7fdeb0bb2b75ae598c804
kawshikbuet17 <kawshik.kumar.paul@gmail.com>
2024-03-11 02:12:19 +0600

    Initial Setup from https://start.spring.io/

---
+++ src/main/java/com/kawshikbuet17/blog/SpringbootBlogAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.kawshikbuet17.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBlogAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kawshikbuet17/blog/SpringbootBlogAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kawshikbuet17.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBlogAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
