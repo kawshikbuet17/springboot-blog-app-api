b7b3a76ef229282156778c2873c3c74c8962a67f
kawshikbuet17 <kawshik.kumar.paul@gmail.com>
2024-03-25 17:11:22 +0600

    Role Based API Access added | Permitted GET methods public access
    Implemented: Only Admin can delete any user

--- src/main/java/com/kawshikbuet17/blog/config/SecurityConfig.java
+++ src/main/java/com/kawshikbuet17/blog/config/SecurityConfig.java
@@ -7,9 +7,11 @@ import lombok.Builder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -21,6 +23,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     @Autowired
     private CustomUserDetailService customUserDetailService;
@@ -39,6 +42,7 @@ public class SecurityConfig {
                         auth->
                                 auth.requestMatchers("/home/**").authenticated()
                                 .requestMatchers("/api/v1/auth/login").permitAll()
+                                .requestMatchers(HttpMethod.GET).permitAll()
                                 .anyRequest().authenticated())
                 .exceptionHandling(ex->ex.authenticationEntryPoint(jwtAuthenticationEntryPoint))
                 .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
--- src/main/java/com/kawshikbuet17/blog/controllers/UserController.java
+++ src/main/java/com/kawshikbuet17/blog/controllers/UserController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -33,6 +34,8 @@ public class UserController {
     }
 
     //DELETE - delete user
+    //Only Admin can do this
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{userId}")
     public ResponseEntity<ApiResponse> deleteUser(@PathVariable("userId") Integer uid){
         this.userService.deleteUser(uid);
